ef50027b06763ca3c11afee8a2a473a673105235
Rahman Yerli <rahmanyerli@gmail.com>
2019-02-23 00:53:03 +1100

    first commit

---
+++ build/precache-manifest.ab66d38366e47783d39344f2b19db3fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "08fb31db63273e5c5eb2",
+    "url": "/racss/static/js/runtime~main.08fb31db.js"
+  },
+  {
+    "revision": "a75184efd31f29908a6f",
+    "url": "/racss/static/js/main.a75184ef.chunk.js"
+  },
+  {
+    "revision": "2a1ab9deb9346fb6ec04",
+    "url": "/racss/static/js/2.2a1ab9de.chunk.js"
+  },
+  {
+    "revision": "a75184efd31f29908a6f",
+    "url": "/racss/static/css/main.daa82ffc.chunk.css"
+  },
+  {
+    "revision": "36b59cead03df452b29682824705a877",
+    "url": "/racss/index.html"
+  }
+];
